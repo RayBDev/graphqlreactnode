0bccba50b4a6f5d50601de21f93ee59181d3f6f5
RayBDev <raybernard1337@gmail.com>
2021-02-17 17:02:11 -0600

    Added first gql query from frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  return <p>Hello</p>;
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+   cache: new InMemoryCache(),
+});
+
+function App(): React.ReactElement {
+   const [posts, setPosts] = useState([]);
+   client
+      .query({
+         query: gql`
+            {
+               allPosts {
+                  id
+                  title
+                  description
+               }
+            }
+         `,
+      })
+      .then((result) => setPosts(result.data.allPosts));
+
+   type Post = {
+      /** post id received from graphql server */
+      id: number;
+      /** post title received from graphql server */
+      title: string;
+      /** post description received from graphql server */
+      description: string;
+   };
+
+   return (
+      <div className="container p-4">
+         <div className="grid md:grid-cols-4 gap-4">
+            {posts.map((post: Post) => (
+               <div className="flex flex-col rounded shadow-md p-5" key={post.id}>
+                  <h4 className="mb-2">{post.title}</h4>
+                  <p>{post.description}</p>
+               </div>
+            ))}
+         </div>
+      </div>
+   );
 }
 
 export default App;
